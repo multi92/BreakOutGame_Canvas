63a4e020dc7fd27d08098977ccd8a064b4283efa
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-08-02 14:52:54 +0200

    player movement

--- breakout.js
+++ breakout.js
@@ -2,20 +2,22 @@ const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
 document.body.prepend(canvas);
 //pikseli svakog objekta kao default vrednost
-const game = { grid: 60 };
-//////////player objekat/////////
+const game = { grid: 60, ani: "" };
+
+/* PLAYER OBJEKAT */
 const player = {
   x: game.grid * 6,
   y: game.grid * 8,
   width: game.grid * 2,
   height: game.grid / 2,
   color: "red",
+  speed: 5,
 };
-////////kraj player objekat//////
+/*  KRAJ PLAYER OBJEKTA*/
 
-/////////key objekat/////////
+/*KEY OBJEKAT*/
 const keyz = { ArrowLeft: false, ArrowRight: false };
-/////kraj key objekat//////
+/*KEY OBJEKAT KRAJ*/
 
 //sirina-visina zavisi od toga kolika je vrednost grida
 canvas.setAttribute("width", game.grid * 15);
@@ -31,17 +33,31 @@ document.addEventListener("keydown", (e) => {
 document.addEventListener("keyup", (e) => {
   if (e.code in keyz) {
     keyz[e.code] = false;
-    console.log(keyz);
+    // console.log(keyz);
   }
 });
 
-///////////crtanje player-a///////////
-draw();
+game.ani = requestAnimationFrame(draw);
+
+/*MOVEMENT FUNCKIJA*/
+function movement() {
+  if (keyz.ArrowLeft) {
+    player.x -= player.speed;
+  }
+  if (keyz.ArrowRight) {
+    player.x += player.speed;
+  }
+}
+/*PALEYR CRTANJE*/
 function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  movement();
   ctx.beginPath();
   ctx.rect(player.x, player.y, player.width, player.height);
   ctx.fillStyle = player.color;
   ctx.fill();
   ctx.closePath();
+  //js animacije(prolazi kroz draw funckiju)
+  game.ani = requestAnimationFrame(draw);
 }
-///////////////////////////////////////
+
