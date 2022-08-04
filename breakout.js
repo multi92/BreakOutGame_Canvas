8995713c597cb30f024b2cd8e7405f9f974e2421
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-08-04 11:58:09 +0200

    bouncing ball

--- breakout.js
+++ breakout.js
@@ -3,7 +3,15 @@ const ctx = canvas.getContext("2d");
 document.body.prepend(canvas);
 //pikseli svakog objekta kao default vrednost
 const game = { grid: 60, ani: "" };
-
+const ball = {
+  x: game.grid * 7,
+  y: game.grid * 5,
+  w: 10,
+  h: 10,
+  color: "blue",
+  dx: 5,
+  dy: 5,
+};
 /* PLAYER OBJEKAT */
 const player = {
   x: game.grid * 6,
@@ -30,19 +38,22 @@ document.addEventListener("keydown", (e) => {
   }
 });
 /*KEYDOWN MOVEMENT KRAJ*/
+
+/*MOUSE MOVEMENT*/
 document.addEventListener("mousemove", (e) => {
   //umesto document.addEventListener mozemo staviti i canvas.addEvenetListener ali cemo onda moci da uzmemo pozicije u canvas board-u
   //ono sto cemo uraditi u addEvent jeste da uzmemo pozicije clientX i clientY
   console.log(e);
 
   const val = e.clientX - canvas.offsetLeft; //moramo uzeti vrednost u canvasu offsetLeft sto vraca broj piksela u levom uglu(delu)
-
   //ako je pomeranje misa u canvas delu
-  if (player.width > 0 && val < canvas.width) {
+  if (val > 0 && val < canvas.width) {
     player.x = val - player.width;
     console.log(player.x);
   }
 });
+/*MOUSE MOVEMENT KRAJ*/
+
 /*KEYUP MOVEMENT*/
 document.addEventListener("keyup", (e) => {
   if (e.code in keyz) {
@@ -51,10 +62,6 @@ document.addEventListener("keyup", (e) => {
   }
 });
 /*KEYUP MOVEMENT KRAJ*/
-
-/*MOUSE MOVEMENT*/
-
-/*MOUSE MOVEMENT KRAJ*/
 
 game.ani = requestAnimationFrame(draw);
 
@@ -68,16 +75,51 @@ function movement() {
   }
 }
 /*MOVEMENT FUNCKIJA KRAJ*/
-/*PALEYR CRTANJE*/
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  movement();
+
+/*BALL MOVEMENT FUNKCIJE*/
+function ballmove() {
+  //okrenuti smer loptice mnozenjem negativne vrednosti(ako je dx 5 icice u -5)
+  if (ball.x > canvas.width || ball.x < 0) {
+    ball.dx *= -1;
+  }
+
+  if (ball.y > canvas.height || ball.y < 0) {
+    ball.dy *= -1;
+  }
+
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+}
+/*BALL MOVEMENT FUNKCIJE KRAJ*/
+
+/*FUNCKIJA DRAWPLAYER*/
+function drawPlayer() {
   ctx.beginPath();
   ctx.rect(player.x, player.y, player.width, player.height);
   ctx.fillStyle = player.color;
   ctx.fill();
   ctx.closePath();
+}
+/*FUNCKIJA DRAWPLAYER KRAJ*/
+
+/*FUNCKIJA DRAWBALL*/
+function drawBall() {
+  ctx.beginPath();
+  ctx.rect(ball.x, ball.y, ball.w, ball.h);
+  ctx.fillStyle = ball.color;
+  ctx.fill();
+  ctx.closePath();
+}
+/*FUNCKIJA DRAWBALL KRAJ*/
+
+/*FUNCKIJA DRAW*/
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  movement();
+  ballmove();
+  drawPlayer();
+  drawBall();
   //js animacije(prolazi kroz draw funckiju)
   game.ani = requestAnimationFrame(draw);
 }
-/*PALEYR CRTANJE KRAJ*/
+/*FUNCKIJA DRAW KRAJ*/
